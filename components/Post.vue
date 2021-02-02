d8e7ca4b4d5add0bea9ae6f338cd89681258a658
Reasonably Selenium <reasonablyselenium@gmail.com>
2021-02-02 18:04:31 -0500

    Fixing website to fit Google Web Design recommendations.

--- components/Post.vue
+++ components/Post.vue
@@ -3,10 +3,14 @@
     <div class="content">
       <img
         v-if="img"
+        width="600"
+        height="200"
         :src="`https://api.aclevo.xyz/assets/` +  img + `?fit=cover&height=200&width=600&quality=80`"
       />
       <nuxt-link :to="`/blog/post/`+ slug">
-        <h2>{{ title }}</h2>
+        <h2>
+          <u>{{ title }}</u>
+        </h2>
       </nuxt-link>
       <div class="content-inner" v-html="content"></div>
     </div>
