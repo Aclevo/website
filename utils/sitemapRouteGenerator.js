ae924706b9668c7673195469f51d4363e869de8d
Reasonably Selenium <reasonablyselenium@gmail.com>
2021-05-28 22:40:28 -0400

    Fixing a sitemap bug

--- utils/sitemapRouteGenerator.js
+++ utils/sitemapRouteGenerator.js
@@ -3,6 +3,12 @@ export default function () {
     const routesToExclude = [] // Add any route you don't want in your sitemap. Potentially get this from an .env file.
     const allRoutes = Array.from(context.generatedRoutes)
     const routes = allRoutes.filter(route => !routesToExclude.includes(route))
+    console.log(routes)
+    console.log(routes.length)
+    for (let i = 0; i < routes.length; i++) {
+      if (!routes[i].endsWith("/")) { routes[i] = routes[i] + "/" } else { routes[i] }
+    }
+    console.log(routes)
 
     this.nuxt.options.sitemap.routes = [...routes]
   })
